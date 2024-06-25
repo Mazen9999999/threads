09c23b172dd4023e6c6af5ac16a1b8ec75df667f
Mazen <mizo2009dodo2011@gmail.com>
2024-06-25 08:58:23 +0300

    profile image update

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,4 +1,229 @@
-"use client"
+// "use client"
+
+// import { useForm } from "react-hook-form";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { UserValidation } from "@/lib/validations/user";
+// import { Button } from "@/components/ui/button"
+// import {
+//     Form,
+//     FormControl,
+//     FormField,
+//     FormItem,
+//     FormLabel,
+//     FormMessage
+// } from "@/components/ui/form"
+// import { Input } from "@/components/ui/input"
+// import { Textarea } from "@/components/ui/textarea"
+// import * as z from "zod";
+// import Image from "next/image";
+// import { ChangeEvent, useState } from "react";
+// import { isBase64Image } from "@/lib/utils";
+// import { useUploadThing } from "@/lib/uploadthing"
+// import { updateUser } from "@/lib/actions/user.action";
+// import { usePathname, useRouter } from "next/navigation";
+
+// interface Props {
+//     user: {
+//         id: string,
+//         objectId: string,
+//         username: string,
+//         name: string,
+//         bio: string,
+//         image: string,
+//     };
+//     btnTitle: string
+// }
+
+// const AccountProfile = ({ user, btnTitle }: Props) => {
+//     const [files, setFiles] = useState<File[]>([]);
+//     const { startUpload } = useUploadThing("media");
+//     const router = useRouter();
+//     const pathname = usePathname();
+
+//     const form = useForm({
+//         resolver: zodResolver(UserValidation),
+//         defaultValues: {
+//             profile_photo: user?.image || "",
+//             name: user?.name || "",
+//             username: user?.username || "",
+//             bio: user?.bio || "",
+//         }
+//     });
+
+
+//     const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) => {
+//         e.preventDefault();
+
+//         const fileReader = new FileReader();
+
+//         if (e.target.files && e.target.files.length > 0) {
+//             const file = e.target.files[0];
+
+//             setFiles(Array.from(e.target.files));
+
+//             if (!file.type.includes('image')) return;
+
+//             fileReader.onload = async (event) => {
+//                 const imageDataUrl = event.target?.result?.toString() || "";
+
+//                 fieldChange(imageDataUrl);
+//             }
+
+//             fileReader.readAsDataURL(file);
+//         }
+//     }
+
+//     const onSubmit = async (values: z.infer<typeof UserValidation>) => {
+//         const blob = values.profile_photo;
+
+//         const hasImageChanged = isBase64Image(blob);
+
+//         if (hasImageChanged) {
+//             const imgRes = await startUpload(files)
+
+//             if (imgRes && imgRes[0].url) {
+//                 values.profile_photo = imgRes[0].url;
+//             }
+//         }
+
+//         await updateUser({
+//             userId: user.id,
+//             username: values.username,
+//             name: values.name,
+//             bio: values.bio,
+//             image: values.profile_photo,
+//             path: pathname
+//         })
+
+//         if (pathname === '/profile/edit') {
+//             router.back();
+//         } else {
+//             router.push("/");
+//         }
+//     }
+
+//     return (
+//         <Form {...form}>
+//             <form onSubmit={form.handleSubmit(onSubmit)}
+//                 className="flex flex-col justify-start gap-10">
+//                 <FormField
+//                     control={form.control}
+//                     name="profile_photo"
+//                     render={({ field }) => (
+//                         <FormItem className="flex items-center gap-4">
+//                             <FormLabel className="account-form_image-label">
+//                                 {field.value ? (
+//                                     <Image
+//                                         src={field.value}
+//                                         alt="profile photo"
+//                                         width={96}
+//                                         height={96}
+//                                         priority
+//                                         className="rounded-full object-contain" />
+//                                 ) : (
+//                                     <Image
+//                                         src="/assets/profile.svg"
+//                                         alt="profile photo"
+//                                         width={24}
+//                                         height={24}
+//                                         className="object-contain" />
+//                                 )}
+//                             </FormLabel>
+//                             <FormControl className="flex-1 text-base-semibold text-gray-200">
+//                                 <Input type="file" accept="image/*" placeholder="Upload a photo"
+//                                     className="account-form_image-input"
+//                                     onChange={(e) => handleImage(e, field.onChange)} />
+//                             </FormControl>
+//                             <FormMessage />
+//                         </FormItem>
+//                     )}
+//                 />
+
+
+//                 <FormField
+//                     control={form.control}
+//                     name="name"
+//                     render={({ field }) => (
+//                         <FormItem className="flex flex-col w-full gap-3">
+//                             <FormLabel className="text-base-semibold text-light-2">
+//                                 Name
+//                             </FormLabel>
+//                             <FormControl>
+//                                 <Input type="text"
+//                                     className="account-form_input no-focus"
+//                                     {...field} />
+//                             </FormControl>
+//                             <FormMessage />
+//                         </FormItem>
+//                     )}
+//                 />
+
+//                 <FormField
+//                     control={form.control}
+//                     name="username"
+//                     render={({ field }) => (
+//                         <FormItem className="flex flex-col w-full gap-3">
+//                             <FormLabel className="text-base-semibold text-light-2">
+//                                 Username
+//                             </FormLabel>
+//                             <FormControl>
+//                                 <Input type="text"
+//                                     className="account-form_input no-focus"
+//                                     {...field} />
+//                             </FormControl>
+//                             <FormMessage />
+//                         </FormItem>
+//                     )}
+//                 />
+//                 <FormField
+//                     control={form.control}
+//                     name="bio"
+//                     render={({ field }) => (
+//                         <FormItem className="flex flex-col w-full gap-3">
+//                             <FormLabel className="text-base-semibold text-light-2">
+//                                 Bio
+//                             </FormLabel>
+//                             <FormControl>
+//                                 <Textarea rows={10}
+//                                     className="account-form_input no-focus"
+//                                     {...field} />
+//                             </FormControl>
+//                             <FormMessage />
+//                         </FormItem>
+//                     )}
+//                 />
+//                 <Button type="submit" className="bg-primary-500">{btnTitle}</Button>
+//             </form>
+//         </Form>
+//     )
+// }
+
+// export default AccountProfile;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"use client";
 
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -21,6 +246,7 @@ import { isBase64Image } from "@/lib/utils";
 import { useUploadThing } from "@/lib/uploadthing"
 import { updateUser } from "@/lib/actions/user.action";
 import { usePathname, useRouter } from "next/navigation";
+import imageCompression from 'browser-image-compression';
 
 interface Props {
     user: {
@@ -50,8 +276,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
         }
     });
 
-
-    const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) => {
+    const handleImage = async (e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) => {
         e.preventDefault();
 
         const fileReader = new FileReader();
@@ -59,19 +284,30 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
         if (e.target.files && e.target.files.length > 0) {
             const file = e.target.files[0];
 
-            setFiles(Array.from(e.target.files));
-
-            if (!file.type.includes('image')) return;
-
-            fileReader.onload = async (event) => {
-                const imageDataUrl = event.target?.result?.toString() || "";
-
-                fieldChange(imageDataUrl);
+            // Check file size (example: limit to 5MB)
+            const MAX_SIZE_MB = 5;
+            if (file.size > MAX_SIZE_MB * 1024 * 1024) {
+                alert("File size exceeds the limit of 5MB.");
+                return;
             }
 
-            fileReader.readAsDataURL(file);
+            try {
+                // Compress image
+                const compressedFile = await imageCompression(file, { maxSizeMB: 1, maxWidthOrHeight: 1024 });
+
+                setFiles([compressedFile]);
+
+                fileReader.onload = (event) => {
+                    const imageDataUrl = event.target?.result?.toString() || "";
+                    fieldChange(imageDataUrl);
+                };
+
+                fileReader.readAsDataURL(compressedFile);
+            } catch (error) {
+                console.error('Error compressing image:', error);
+            }
         }
-    }
+    };
 
     const onSubmit = async (values: z.infer<typeof UserValidation>) => {
         const blob = values.profile_photo;
@@ -79,7 +315,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
         const hasImageChanged = isBase64Image(blob);
 
         if (hasImageChanged) {
-            const imgRes = await startUpload(files)
+            const imgRes = await startUpload(files);
 
             if (imgRes && imgRes[0].url) {
                 values.profile_photo = imgRes[0].url;
@@ -93,14 +329,14 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
             bio: values.bio,
             image: values.profile_photo,
             path: pathname
-        })
+        });
 
         if (pathname === '/profile/edit') {
             router.back();
         } else {
             router.push("/");
         }
-    }
+    };
 
     return (
         <Form {...form}>
@@ -138,8 +374,6 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                         </FormItem>
                     )}
                 />
-
-
                 <FormField
                     control={form.control}
                     name="name"
@@ -157,7 +391,6 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                         </FormItem>
                     )}
                 />
-
                 <FormField
                     control={form.control}
                     name="username"
